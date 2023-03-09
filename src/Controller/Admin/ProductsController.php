64fb6ffc663b097d3a6cfea7f1ef38a8d5ae63b7
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-03-09 18:09:19 +0100

    Persist images in method Add of ProductController

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Images;
 use App\Entity\Products;
 use App\Form\ProductsFormType;
+use App\Service\PictureService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,7 +23,8 @@ class ProductsController extends AbstractController
     }
 
     #[Route('/ajout', name: 'add')]
-    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, 
+    PictureService $pictureService): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
@@ -36,6 +39,21 @@ class ProductsController extends AbstractController
 
         //On vérifie si le formulaire est soumis Et valide
         if($productForm->isSubmitted() && $productForm->isValid()){
+            //On récupère les images 
+            $images = $productForm->get('image')->getData();
+            
+            foreach($images as $image){
+                // On définit le dossier de déstination
+                $folder = 'products';
+
+                // On appel le service d'ajout
+                $fichier = $pictureService->add($image, $folder, 300, 300);
+
+                $img = new Images();
+                $img->setName($fichier);
+                $product->addImage($img);
+            }
+
             // On génère le slug
             $slug = $slugger->slug($product->getName());
             $product->setSlug($slug);
