0fc96f858c5b63abf1b706a5e8fca2fed9eae108
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-19 14:01:26 +0200

    Add Categories CRUD

--- src/Controller/Admin/CategoriesController.php
+++ src/Controller/Admin/CategoriesController.php
@@ -1,20 +1,78 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller;
 
+use App\Entity\Categories;
+use App\Form\CategoriesType;
 use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin/categories', name: 'admin_categories_')]
 class CategoriesController extends AbstractController
 {
-  #[Route('/', name: 'index')]
-  public function index(CategoriesRepository $categoriesRepository): Response
-  {
-    $categories = $categoriesRepository->findBy([], ['categoryOrder' => 'asc']);
-    
-    return $this->render('admin/categories/index.html.twig', compact('categories'));
-  }
+    #[Route('/', name: 'index')]
+    public function index(CategoriesRepository $categoriesRepository): Response
+    {
+        $categories = $categoriesRepository->findBy([], ['categoryOrder' => 'asc']);
+        
+        return $this->render('admin/categories/index.html.twig', compact('categories'));
+    }
+
+    #[Route('/new', name: 'app_categories_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, CategoriesRepository $categoriesRepository): Response
+    {
+        $category = new Categories();
+        $form = $this->createForm(CategoriesType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categoriesRepository->save($category, true);
+
+            return $this->redirectToRoute('app_categories_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('categories/new.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_categories_show', methods: ['GET'])]
+    public function show(Categories $category): Response
+    {
+        return $this->render('categories/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_categories_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Categories $category, CategoriesRepository $categoriesRepository): Response
+    {
+        $form = $this->createForm(CategoriesType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categoriesRepository->save($category, true);
+
+            return $this->redirectToRoute('app_categories_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('categories/edit.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_categories_delete', methods: ['POST'])]
+    public function delete(Request $request, Categories $category, CategoriesRepository $categoriesRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $categoriesRepository->remove($category, true);
+        }
+
+        return $this->redirectToRoute('app_categories_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
