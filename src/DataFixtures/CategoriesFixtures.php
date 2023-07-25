c3fe54f2c6ed9db194840d9ecc33f994634ce354
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-25 20:30:59 +0200

    fixture CategoryOrder

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -15,25 +15,26 @@ class CategoriesFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $parent = $this->createCategory('Informatique', null, $manager);
+        $parent = $this->createCategory('Informatique', null, $manager, 0);
         
-        $this->createCategory('Ordinateurs potables', $parent, $manager);
-        $this->createCategory('Ecrans', $parent, $manager);
-        $this->createCategory('Souris', $parent, $manager);
+        $this->createCategory('Ordinateurs potables', $parent, $manager, 1);
+        $this->createCategory('Ecrans', $parent, $manager, 2);
+        $this->createCategory('Souris', $parent, $manager, 3);
 
-        $parent = $this->createCategory('Mode', null, $manager);
+        $parent = $this->createCategory('Mode', null, $manager, 4);
         
-        $this->createCategory('Homme', $parent, $manager);
-        $this->createCategory('Femme', $parent, $manager);
-        $this->createCategory('Enfant', $parent, $manager);
+        $this->createCategory('Homme', $parent, $manager, 5);
+        $this->createCategory('Femme', $parent, $manager, 6);
+        $this->createCategory('Enfant', $parent, $manager, 7);
         
         $manager->flush();
     }
 
-    public function createCategory(string $name, Categories $parent = null, ObjectManager $manager)
+    public function createCategory(string $name, Categories $parent = null, ObjectManager $manager, int $categoryOrder)
     {
         $category = new Categories();
         $category->setName($name);
+        $category->setcategoryOrder($categoryOrder);
         $category->setSlug($this->slugger->slug($category->getName())->lower());
         $category->setParent($parent);
         $manager->persist($category);
