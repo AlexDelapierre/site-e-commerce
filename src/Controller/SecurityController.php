ec9b7aa7bd8ce531b02192e62805ddf7c4928f2a
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-12-29 20:16:45 +0100

    Password recovery system [WIP]

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Form\ResetPasswordRequestFormType;
+use App\Repository\UsersRepository;
+use App\Service\SendMailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -31,5 +38,67 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    // Récupération de mot de passe
+    #[Route('/oubli-pass', name:'forgotten_password')]
+    public function forgottenPassword(
+        Request $request, 
+        UsersRepository $usersRepository, 
+        TokenGeneratorInterface $tokenGeneratorInterface,
+        EntityManagerInterface $entityManagerInterface,
+        SendMailService $mail
+        ): Response
+    {
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //On va chercher l'utilisateur par son email
+            $user = $usersRepository->findOneByEmail($form->get('email')->getData());
+
+            // On vérifie si on a un utilisateur
+            if ($user) {
+                // On génère un token de réinitialisation
+                $token = $tokenGeneratorInterface->generateToken();
+                $user->setResetToken($token);
+                $entityManagerInterface->persist($user);
+                $entityManagerInterface->flush();
+
+                // On génère un lien de réinitialisation du mot de passe
+                $url = $this->generateUrl('reset_pass', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+                dd($url);
+
+                //On crée les données du mail
+                $context = compact('url', 'user');
+
+                //Envoi du mail
+                $mail->send(
+                    'no-reply@e-commerce.fr',
+                    $user->getEmail(),
+                    'Réinitialisation du mot de passe',
+                    'password_reset',
+                    $context
+                );
+
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');
+
+            }
+            // $user est null
+            $this->addFlash('danger', 'Un problème est survenu');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password_request.html.twig', [
+            'requestPassForm' => $form->createView()
+        ]);
+    }
+
+    #[Route('/oubli-pass/{token}', name:'reset_pass')]
+    public function resetPass(): Response
+    {
+
     }
 }
